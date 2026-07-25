7daf3a69270dbd5fdc234d4ab6a9756cc1571994
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2026-07-25 17:13:13 +0200

    feat(import): add import job hooks

--- client/src/data-provider/index.ts
+++ client/src/data-provider/index.ts
@@ -3,6 +3,8 @@ export * from './Agents';
 export * from './Endpoints';
 export * from './Skills';
 export * from './Files';
+/* Import */
+export * from './Import';
 /* Memories */
 export * from './Memories';
 export * from './Messages';
